dc5e68a3e69d3f0dd1ac7ea3740d943153e280ed
Luís Fernando Laguardia <laguardialuis@gmail.com>
2023-07-17 13:16:14 -0300

    preps to general view

--- tabs/general_view.py
+++ tabs/general_view.py
@@ -1,13 +1,33 @@
-from dash import Dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from modules import predictor
 
-# ===== Layout ===== #
+# ===== Helper functions ===== #
+def create_posture_quality_graph(state):
+    pass
+
+def create_posture_balance_graph(data):
+    pass
+
+def create_graphs():
+    day, state, data = predictor.get_last_active_day_data()
+    posture_quality_graph = create_posture_quality_graph(state)
+    posture_balance_graph = create_posture_balance_graph(data)
+
+    return day, posture_quality_graph, posture_balance_graph
+
+# ===== Variables ===== #
+day, posture_quality_graph, posture_balance_graph = create_graphs()
+
+# ===== Base layout ===== #
 layout = html.Div([
     dbc.Row(justify="center", children=[
         dbc.Col([
             html.H2("General View", className="tabTitle"),
-            html.P("14/07", id="dateText"),
+            html.P(day, id="dateText"),
 
             # Graphs
             html.H2("Time Seated"),
@@ -15,8 +35,9 @@ layout = html.Div([
 
             html.Br(),
             html.H2("Posture Quality"),
-            dcc.Graph(id="postureQualityGraph"),
-            dcc.Graph(id="postureBalanceGraph")
+            dcc.Graph(id="postureQualityGraph", figure=posture_quality_graph),
+            dcc.Graph(id="postureBalanceGraph", figure=posture_balance_graph),
         ], width=8, style={"textAlign": "center", "align-content": "center"}),
     ]),
 ])
+
