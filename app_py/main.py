b5d8da7f411a8fe185bf0ac24122f3134875a820
Bissiatti <emanulebissiatti@outlook.com.br>
2023-11-18 10:01:29 -0300

    add login

--- app_py/main.py
+++ app_py/main.py
@@ -1,8 +1,12 @@
-from flask import Flask, send_from_directory, json, jsonify
-
+from flask import Flask, send_from_directory, json, jsonify, request, render_template,request, redirect, flash
+import json
 from get_data import *
 
+
 app = Flask(__name__, static_folder="public")
+with open('secret.json') as config_file:
+    config = json.load(config_file)
+app.config['SECRET_KEY'] = config['SECRET_KEY']
 
 @app.route("/")
 def index():
@@ -11,6 +15,7 @@ def index():
 
 @app.route("/<path:path>")
 def static_file(path):
+    print(path)
     # Retorna qualquer outro arquivo estático da pasta public
     return app.send_static_file(path)
 
@@ -22,6 +27,33 @@ def data():
     data_json = jsonify(data)
     # retornar os dados em JSON como resposta
     return data_json
+
+@app.route("/admin")
+def admin():
+    return render_template('login.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    
+    name = request.form.get('username')
+    password = request.form.get('password')
+
+    if name == config['ADMIN_USER'] and password == config['ADMIN_PASSWORD']:
+        return render_template('submit.html')
+    else:
+        flash('Login Unsuccessful. Please check username and password')
+        return redirect('/admin')
+    
+@app.route('/submit',methods=['POST'])
+def submit():
+    print('submit')
+    name = request.form.get('name')
+    birth = request.form.get('birthdate')
+    height = request.form.get('height')
+    weight = request.form.get('weight')
+    
+    print(name, birth, height, weight)
+    return redirect('/admin')
 
 
 if __name__ == "__main__":
--- app_py/public/script.js
+++ app_py/public/script.js
@@ -1,34 +1,126 @@
 class App{
     constructor(){
-        this.div = document.getElementById("content");;
+        this.div = document.getElementById("content");
+        this.div.innerHTML = ''
+        this.graph = document.createElement("div");
+        this.graph.id = "myPlot";
+        this.div.appendChild(this.graph);
+        this.dayData = {};
+        this.lastMomentData = {};
+        this.lastMoment = 0;
+        this.times = [];
+        this.chart = {};
+        this.options = {
+            series: {},
+            chart: {
+                id: 'realtime',
+                height: 350,
+                type: 'line',
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            title: {
+                text: 'Dynamic Updating Chart',
+                align: 'left'
+            },
+            markers: {
+                size: 0
+            },
+            xaxis: {
+                type: 'datetime',
+            },
+            yaxis: {
+                max: 5000
+            },
+            legend: {
+                show: false
+            }
+        };
+        
+    }
+
+    convertData(data){
+        var series = [];
+        // Transform the data
+        Object.keys(data).forEach(function(timestamp) {
+            var values = data[timestamp].P;
+
+            values.forEach(function(value, index) {
+                if (!series[index]) {
+                    series[index] = { name: 'Series ' + (index + 1), data: [] };
+                }
+                series[index].data.push({ x: new Date(parseInt(timestamp)), y: value });
+            });
+        });
+
+        this.options.series = series;
+
+        console.table(series);
+
     }
 
     getDataDay(){
         fetch("/data")
         .then(response => response.json())
         .then(data => {
-            data = data[0]
-            // const keys = Object.keys(data)
-            // const l = keys.length - 1
-            // this.plotActualData(data[keys[l]]);
-            this.renderData(data);
+            console.log(data);
+            this.times = Object.keys(data);
+            this.times = Array.from(this.times);
+            const l = this.times.length - 1;
+            this.convertData(data);
+            this.dayData = data;
+            if (this.lastMoment < this.times[l]){ 
+                this.plotTimesSeriesData();
+            }
+            this.lastMomentData = data[this.times[l]];
+            this.lastMoment = this.times[l];
         });
     }
+    
 
-    renderData(data){
-        console.log(data);
-        // let html = "";
-        // for(let i = 0; i < data.length; i++){
-        //     html += `<p>${data[i].name}</p>`;
-        // }
-        // this.div.innerHTML = html;
+    plotTimesSeriesData(){;
+        this.chart = new ApexCharts(this.div, this.options);
+        this.chart.render();
     }
 
-    plotActualData(data){
-        
-    }
+    // plotActualData(){
+    //     const data = [{
+    //         x: this.lastMomentData['p'].length,
+    //         y: this.lastMomentData['p'],
+    //         type: "bar",
+    //         orientation:"v",
+    //         marker: {color:"rgba(0,0,255)"}
+    //     }];
+
+    //     const layout = {title:"Valor dos sensores de pressão no último momento"};
+
+    //     Plotly.newPlot("myPlot", data, layout);
+    // }
 }
 
 let app = new App();
 
 app.getDataDay();
+
+
+window.setInterval(function() {
+    app.getDataDay();
+    // this.chart.updateSeries([ { data: app.options.series }]);
+}, 10000);
